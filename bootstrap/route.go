e2687d471d3a6805db89e19ad0b584e57d64447f
feilongjump <feilongjump@qq.com>
2021-10-14 18:09:17 +0800

    CORS

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,16 +1,19 @@
 package bootstrap
 
 import (
-    "github.com/gin-gonic/gin"
-    "leek-api/routes"
+	"github.com/gin-gonic/gin"
+	"leek-api/app/http/middlewares"
+	"leek-api/routes"
 )
 
 // SetupRoute 路由初始化
 func SetupRoute() {
 
-    r := gin.Default()
+	r := gin.Default()
 
-    routes.RegisterApiRoutes(r)
+	r.Use(middlewares.Cors())
 
-    r.Run()
+	routes.RegisterApiRoutes(r)
+
+	r.Run()
 }
