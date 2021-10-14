3e2a124bcf61be67201a81fe236e3465e24d108a
feilongjump <feilongjump@qq.com>
2021-10-14 18:08:25 +0800

    :fire: remove code

--- app/http/resources/resource.go
+++ app/http/resources/resource.go
@@ -1,7 +1,6 @@
 package resources
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
@@ -16,7 +15,7 @@ func ResponseValidationFailed(c *gin.Context, err error) {
 
 // ResponseForSQLError 处理 SQL 错误并返回
 func ResponseForSQLError(c *gin.Context, err error) {
-	fmt.Println(err == gorm.ErrRegistered)
+
 	if err == gorm.ErrRecordNotFound {
 		// 数据未找到
 		c.JSON(http.StatusNotFound, gin.H{
