8848b63758f792158f778484647885bdeb26bc39
feilongjump <feilongjump@qq.com>
2022-07-21 07:47:15 +0800

    Add SITE_URL

--- config/app.php
+++ config/app.php
@@ -58,6 +58,8 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'site_url' => env('SITE_URL', 'http://localhost'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
